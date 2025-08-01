405f2b9e2b2e97ce4bc1f935d42be84951f12d62
rohitwebstep <rohitwebstep@gmail.com>
2025-08-01 10:41:35 +0530

    update - 1

--- src/backend/app/Services/CountryService.php
+++ src/backend/app/Services/CountryService.php
@@ -9,41 +9,57 @@ use Illuminate\Database\Eloquent\Collection;
 /**
  * @implements IBaseService<Country>
  */
-class CountryService implements IBaseService {
+class CountryService implements IBaseService
+{
     public function __construct(
         private Country $country,
     ) {}
 
-    public function getByCode(?string $countryCode): Country {
-        return $countryCode !== null ? $this->country->where('country_code', $countryCode)->first() : $countryCode;
+    /*
+        public function getByCode(?string $countryCode): Country
+        {
+            return $countryCode !== null ? $this->country->where('country_code', $countryCode)->first() : $countryCode;
+        }
+    */
+
+    public function getByCode(?string $countryCode): Country
+    {
+        return $countryCode !== null
+            ? $this->country->where('country_code', $countryCode)->first()
+            : $this->country->first();
     }
 
-    public function getById(int $id): Country {
+    public function getById(int $id): Country
+    {
         throw new \Exception('Method getById() not yet implemented.');
     }
 
     /**
      * @param array<string, mixed> $data
      */
-    public function create(array $data): Country {
+    public function create(array $data): Country
+    {
         throw new \Exception('Method create() not yet implemented.');
     }
 
     /**
      * @param array<string, mixed> $data
      */
-    public function update($model, array $data): Country {
+    public function update($model, array $data): Country
+    {
         throw new \Exception('Method update() not yet implemented.');
     }
 
-    public function delete($model): ?bool {
+    public function delete($model): ?bool
+    {
         throw new \Exception('Method delete() not yet implemented.');
     }
 
     /**
      * @return Collection<int, Country>
      */
-    public function getAll(?int $limit = null): Collection {
+    public function getAll(?int $limit = null): Collection
+    {
         throw new \Exception('Method getAll() not yet implemented.');
     }
 }
