af4dadad45c23205f5edadf029e4439133a551c2
rohitwebstep <rohitwebstep@gmail.com>
2025-08-20 17:56:10 +0530

    update

--- src/backend/app/Utils/DemoCompany.php
+++ src/backend/app/Utils/DemoCompany.php
@@ -5,8 +5,7 @@ namespace App\Utils;
 class DemoCompany {
     public const DEMO_COMPANY_ID = 1;
     public const DEMO_COMPANY_NAME = 'Demo Company';
-    // public const DEMO_COMPANY_DATABASE_NAME = 'DemoCompany_1';
-    public const DEMO_COMPANY_DATABASE_NAME = 'micro';
+    public const DEMO_COMPANY_DATABASE_NAME = 'DemoCompany_1';
     public const DEMO_COMPANY_PASSWORD = '123123';
     public const DEMO_COMPANY_CURRENCY = 'TSZ';
     public const DEMO_COMPANY_CURRENCY_FACTOR = 1000.0; // 1 USD = 1000 TSZ
