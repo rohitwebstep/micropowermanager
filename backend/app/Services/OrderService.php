488f8704924836d6d72e6514c0c7aabd91c9bada
sisodiadude <dudesisodia@gmail.com>
2026-02-23 17:55:25 +0530

    update - 5

--- backend/app/Services/OrderService.php
+++ backend/app/Services/OrderService.php
@@ -169,7 +169,7 @@ class OrderService implements IBaseService
 
                 if (!$person instanceof Person) {
                     $customerRequestData = [
-                        'national_id_number'  => '123123',
+                        'national_id_number'  => $data['national_id_number'] ?? null,
                         'name'                => $data['first_name'] ?? null,
                         'serial_number'       => null,
                         'meter_type'          => $meterTypeId ?? 0,
